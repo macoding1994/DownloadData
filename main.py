7a027ad8dd1c0709a412610cf53ea8bb4c92741f
macoding1994 <macoding@163.com>
2019-12-03 02:14:32 +0800

    第三-未完成

--- main.py
+++ main.py
@@ -9,27 +9,37 @@ import sys
 import threading
 import time
 import requests
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot,pyqtSignal
 from PyQt5.QtGui import QTextCursor
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from selenium.webdriver.support.wait import WebDriverWait
 
 from Ui_main import Ui_MainWindow
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 import datetime
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    a = pyqtSignal()
+    b = pyqtSignal()
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.dirurl = None
         self.dirlist = []
+        self.stationlist = []
 
         self.url = 'https://cmr.earthdata.nasa.gov/search/concepts/{}?pretty=true'
         self.headers = {
             "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, likeGecko) Chrome / 73.0.3683.86 Safari/537.36",
             "Origin: https: // search.earthdata.nasa.gov"
         }
+        self.a.connect(self.findno1)
+        self.b.connect(self.findno2)
 
     def show_info(self, str):
         self.textBrowser.append(str)
@@ -62,38 +72,40 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         t.start()
 
     def qiang(self):
-        length = len(self.dirlist)
-        filelist = os.listdir(self.dirurl)
-        for url in self.dirlist:
-            if not url+'.txt' in filelist:
-                res = requests.get(self.url.format(url), headers=self.headers)
-                if res.status_code == 200:
-                    with open('{}\{}.txt'.format(self.dirurl,url),'w',encoding='utf-8') as f:
-                        f.write(res.text)
-                    self.show_info('{}.txt  已下载，目前还剩{}未下载'.format(url,length - self.dirlist.index(url) -1))
-                else:
-                    self.show_info('{}.txt  下载失败，已添加错误列表'.format(url))
-                    with open('{}\error.txt'.format(self.dirurl),'a',encoding='utf-8') as f:
-                        f.write(url)
-            else:
-                self.show_info('{}.txt  已下载'.format(url))
+        while 1:
+            try:
+                print(self.driver.find_element_by_xpath('//*[@id="tooltipDialog"]/div[1]/div/div/b').text)
+            except Exception:
+                pass
 
     def findno(self):
-        while 1:
-            self.driver.switch_to.window(self.driver.window_handles[-1])
-            length = self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/section/header/div[3]/div/div/span/span').text
-            imglist = self.driver.find_elements_by_xpath('//*[@id="app"]/div[1]/div/div/section/section/div/div/div[1]/div[2]/div/div/div/div/ul/li/div[2]/div[1]/img')
-            self.show_info('已发现{}数据包'.format(len(imglist)))
-            if not len(imglist) < int(length):
-                break
-        imglist = self.driver.find_elements_by_xpath(
-            '//*[@id="app"]/div[1]/div/div/section/section/div/div/div[1]/div[2]/div/div/div/div/ul/li/div[2]/div[1]/img')
-        self.dirlist = []
-        for img in imglist:
-            self.show_info(img.get_attribute('src').rsplit('/')[-1].rsplit('?')[0])
-            self.dirlist.append(img.get_attribute('src').rsplit('/')[-1].rsplit('?')[0])
+        self.stationlist = []
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        marklist = self.driver.find_elements_by_tag_name('path')
+        for mark in marklist:
+            if not mark.get_attribute('fill') == 'rgb(255, 0, 0)':
+                ActionChains(self.driver).move_to_element(mark).perform()
+                self.mark = True
+                self.a.emit()
+                time.sleep(2)
         else:
-            self.show_info('该页面发现{}个数据包'.format(len(self.dirlist)))
+            self.mark = False
+            with open('station.txt','a') as f:
+                for station in self.stationlist:
+                    f.write(str(station) + '\n')
+            self.show_info('站点已添加')
+
+    def findno1(self):
+        while self.mark:
+            try:
+                data = self.driver.find_element_by_xpath('//*[@id="tooltipDialog"]/div[1]/div/div/b').text
+                if data:
+                    self.stationlist.append(data)
+            except Exception:
+                pass
+
+    def findno2(self):
+        self.mark = False
 
     @pyqtSlot()
     def on_pushButton_4_clicked(self):
@@ -101,9 +113,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': self.dirurl}
         options.add_experimental_option('prefs', prefs)
         self.driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver.exe')
+        self.driver.implicitly_wait(3)
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
         }
+        self.driver.get('https://www.ndbc.noaa.gov/')
 
 
 def main():
