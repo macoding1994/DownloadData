a623b5e7fd590834b5ba7a692a22152480222406
macoding1997 <macoding@163.com>
2019-12-03 14:58:38 +0800

    第三

--- Ui_main.py
+++ Ui_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Administrator\Desktop\ZKFLproject\pyqt\DownloadData\main.ui'
+# Form implementation generated from reading ui file 'C:\Users\Administrator\Desktop\ZKFLproject\crawler\DownloadData\main.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.1
 #
@@ -22,18 +22,21 @@ class Ui_MainWindow(object):
         self.pushButton_2 = QtWidgets.QPushButton(self.centralWidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.gridLayout.addWidget(self.pushButton_5, 0, 2, 1, 1)
         self.pushButton_3 = QtWidgets.QPushButton(self.centralWidget)
         self.pushButton_3.setObjectName("pushButton_3")
-        self.gridLayout.addWidget(self.pushButton_3, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_3, 0, 3, 1, 1)
         self.pushButton_4 = QtWidgets.QPushButton(self.centralWidget)
         self.pushButton_4.setObjectName("pushButton_4")
-        self.gridLayout.addWidget(self.pushButton_4, 0, 3, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_4, 0, 4, 1, 1)
         self.lineEdit = QtWidgets.QLineEdit(self.centralWidget)
         self.lineEdit.setObjectName("lineEdit")
-        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 4)
+        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 5)
         self.textBrowser = QtWidgets.QTextBrowser(self.centralWidget)
         self.textBrowser.setObjectName("textBrowser")
-        self.gridLayout.addWidget(self.textBrowser, 2, 0, 1, 4)
+        self.gridLayout.addWidget(self.textBrowser, 2, 0, 1, 5)
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
@@ -44,6 +47,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "生产文件夹"))
         self.pushButton_2.setText(_translate("MainWindow", "显示数据量"))
+        self.pushButton_5.setText(_translate("MainWindow", "数据追踪"))
         self.pushButton_3.setText(_translate("MainWindow", "下载"))
         self.pushButton_4.setText(_translate("MainWindow", "开启浏览器"))
 
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 import threading
 import time
 import requests
-from PyQt5.QtCore import pyqtSlot,pyqtSignal
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QTimer
 from PyQt5.QtGui import QTextCursor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from selenium.webdriver.support.wait import WebDriverWait
@@ -25,7 +25,7 @@ import datetime
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     a = pyqtSignal()
-    b = pyqtSignal()
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
@@ -33,16 +33,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.dirlist = []
         self.stationlist = []
 
-        self.url = 'https://cmr.earthdata.nasa.gov/search/concepts/{}?pretty=true'
+        self.url = 'https://www.ndbc.noaa.gov/station_page.php?station={}'
         self.headers = {
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, likeGecko) Chrome / 73.0.3683.86 Safari/537.36",
-            "Origin: https: // search.earthdata.nasa.gov"
+            "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, likeGecko) Chrome / 73.0.3683.86 Safari/537.36"
         }
-        self.a.connect(self.findno1)
-        self.b.connect(self.findno2)
+        self.countno = 0
+        self.a.connect(self.on_pushButton_5_clicked)
 
     def show_info(self, str):
-        self.textBrowser.append(str)
+        self.countno += 1
+        self.textBrowser.append('{}:{}'.format(self.countno, str))
         self.textBrowser.moveCursor(QTextCursor.End)
 
     @pyqtSlot()
@@ -67,45 +67,58 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot()
     def on_pushButton_3_clicked(self):
-        t = threading.Thread(target=self.qiang)
+        t = threading.Thread(target=self.download)
         t.setDaemon(True)
         t.start()
 
+    def download(self):
+        savelist = os.listdir(self.dirurl)
+        try:
+            with open('{}\\faile.txt'.format(self.dirurl), 'r', encoding='utf-8') as f:
+                failelist = f.readlines()
+        except Exception:
+            failelist = []
+        for station in self.stationlist:
+            name = station.split(' ')[-1]
+            if not (name + '.txt' in savelist or name+'\n' in failelist):
+                res = requests.get(self.url.format(name), headers=self.headers)
+                if res.status_code == 200:
+                    self.success(name, res.text)
+                    self.show_info('{}成功'.format(name))
+                else:
+                    self.faile(name)
+                    self.show_info('{}失败'.format(name))
+            else:
+                self.show_info('{}过滤'.format(name))
+
+    def success(self, station, str):
+        with open('{}\{}.txt'.format(self.dirurl, station), 'w', encoding='utf-8') as f:
+            f.write(str)
+
+    def faile(self, station):
+        with open('{}\\faile.txt'.format(self.dirurl), 'a', encoding='utf-8') as f:
+            f.write(str(station) + '\n')
+
     def qiang(self):
-        while 1:
-            try:
-                print(self.driver.find_element_by_xpath('//*[@id="tooltipDialog"]/div[1]/div/div/b').text)
-            except Exception:
-                pass
-
-    def findno(self):
-        self.stationlist = []
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-        marklist = self.driver.find_elements_by_tag_name('path')
-        for mark in marklist:
-            if not mark.get_attribute('fill') == 'rgb(255, 0, 0)':
-                ActionChains(self.driver).move_to_element(mark).perform()
-                self.mark = True
-                self.a.emit()
-                time.sleep(2)
-        else:
-            self.mark = False
-            with open('station.txt','a') as f:
-                for station in self.stationlist:
-                    f.write(str(station) + '\n')
-            self.show_info('站点已添加')
-
-    def findno1(self):
-        while self.mark:
+        while self.start:
             try:
                 data = self.driver.find_element_by_xpath('//*[@id="tooltipDialog"]/div[1]/div/div/b').text
                 if data:
                     self.stationlist.append(data)
+                    self.show_info(str(data))
             except Exception:
                 pass
 
-    def findno2(self):
-        self.mark = False
+    def findno(self):
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        marklist = self.driver.find_elements_by_tag_name('path')
+        for mark in marklist:
+            if not mark.get_attribute('fill') == 'rgb(255, 0, 0)':
+                for _ in range(5):
+                    ActionChains(self.driver).move_to_element(mark).perform()
+                    time.sleep(0.5)
+        else:
+            self.a.emit()
 
     @pyqtSlot()
     def on_pushButton_4_clicked(self):
@@ -118,6 +131,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
         }
         self.driver.get('https://www.ndbc.noaa.gov/')
+
+    @pyqtSlot()
+    def on_pushButton_5_clicked(self):
+        if self.pushButton_5.text() == '数据追踪':
+            self.start = True
+            t = threading.Thread(target=self.qiang)
+            t.setDaemon(True)
+            t.start()
+            self.pushButton_5.setText('暂停')
+        else:
+            self.pushButton_5.setText('数据追踪')
+            self.start = False
+            self.countno = 0
+            self.textBrowser.clear()
+            self.stationlist = list(set(self.stationlist))
+            for station in self.stationlist:
+                self.show_info('{}--{}'.format(self.stationlist.index(station), station))
 
 
 def main():
